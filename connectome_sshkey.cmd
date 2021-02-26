42e53cd482b9446324f4e72eb3e8ff89299247eb
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-02-26 20:09:15 +0900

    Update connectome_sshkey.cmd

--- connectome_sshkey.cmd
+++ connectome_sshkey.cmd
@@ -2,7 +2,7 @@ set id=%1
 set pw=%2
 
 ssh-keygen -t rsa
-type %systemdrive%%homepath%\.ssh\id_rsa.pub | ssh %id%@147.47.200.169 -o StrictHostKeyChecking=no 'umask 077;mkdir ~/.ssh;cat >> ~/.ssh/authorized_keys'
+type '%systemdrive%%homepath%\.ssh\id_rsa.pub' | ssh %id%@147.47.200.169 -o StrictHostKeyChecking=no 'umask 077;mkdir ~/.ssh;cat >> ~/.ssh/authorized_keys'
 
 ssh -T -A -o StrictHostKeyChecking=no %id%@147.47.200.169 << EOF
 bash
