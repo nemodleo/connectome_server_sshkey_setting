6d2a222e758fe6b518ed4740001d7f82eebf8fdf
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-02-26 20:03:01 +0900

    Update connectome_sshkey.cmd

--- connectome_sshkey.cmd
+++ connectome_sshkey.cmd
@@ -1,10 +1,10 @@
-id=%1
-pw=%2
+set id=%1
+set pw=%2
 
 ssh-keygen -t rsa
-cat %systemdrive%%homepath%/.ssh/id_rsa.pub | ssh $id@147.47.200.169 -o StrictHostKeyChecking=no 'umask 077;mkdir ~/.ssh;cat >> ~/.ssh/authorized_keys'
+type %systemdrive%%homepath%\.ssh\id_rsa.pub | ssh %id%@147.47.200.169 -o StrictHostKeyChecking=no 'umask 077;mkdir ~/.ssh;cat >> ~/.ssh/authorized_keys'
 
-ssh -T -A -o StrictHostKeyChecking=no $id@147.47.200.169 << EOF
+ssh -T -A -o StrictHostKeyChecking=no %id%@147.47.200.169 << EOF
 bash
 nodes="master node1 node2 storage"
 for node in \$nodes; do 
