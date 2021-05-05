8de9eaebe4b298e0dcd1d839986c0b068ac78031
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-05-05 21:34:23 +0900

    Update connectome_sshkey.bat

--- connectome_sshkey.bat
+++ connectome_sshkey.bat
@@ -7,5 +7,5 @@ set pw=%2
 echo "%userprofile%\.ssh\id_rsa.pub"
 type "%userprofile%\.ssh\id_rsa.pub"
 
-type "%userprofile%\.ssh\id_rsa.pub" | ssh %id%@147.47.200.169 -o StrictHostKeyChecking=no "umask 077; rm -r ~/.ssh; mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys"
+type "%userprofile%\.ssh\id_rsa.pub" | ssh %id%@147.47.200.169 -o StrictHostKeyChecking=no "mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys"
 ssh -T -A -o StrictHostKeyChecking=no %id%@147.47.200.169 "nodes=(master node1 node2 storage); for node in ${nodes[@]}; do cat ~/.ssh/authorized_keys | sshpass -p %pw% ssh %id%@$node -o StrictHostKeyChecking=no 'umask 077; rm -r ~/.ssh; mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys'; done"
